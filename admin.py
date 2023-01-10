d82d12a6bc3cd829e8a2e74faefd43662f789a89
Nirajan Dhakal <Nirajan Dhakal>
2023-01-10 16:47:04 +0545

    loan approve function added

--- admin.py
+++ admin.py
@@ -27,14 +27,56 @@ def customerDetails():
                         print(value.ljust(20), end="")
 
 
-
-def approveLoan():
+def displaLoanRequest():
     print("................Loan request..............\n")
-    with open("loan_request.txt",'w') as fp:
+    with open("loan_request.txt",'r') as fp:
+        heading = ['UID', 'LoanType', 'DATE', 'AMOUNT']
+        for i in heading:
+            if (i == heading[len(heading) - 1]):
+                print(i.center(20))
+            else:
+                print(i.center(20), end="")
+        print('\t------------------------------------------------------------------------')
         for data_db in fp:
-            print(data_db)
-    print("\nApprove Loan using their respective user ID\n")
-    cust_ID = input("Enter customer id ")
+            list_data = data_db.split(',')
+            for data in list_data:
+                if (data == list_data[len(list_data) - 1]):
+                    print(data.center(20))
+                else:
+                    print(data.center(20), end="")
+    acceptLoan()
+
+
+
+def acceptLoan():
+    print("\nApprove Loan using respective user ID\n")
+    cust_ID = input("Enter customer id: ")
+    with open("loan_request.txt","r") as file:
+        for value in file:
+            ldata = value.split(',')
+            if cust_ID in ldata:
+                loanID = str(generateLoanID())
+                ldata.insert(0,loanID)
+                with open('loan.txt','a') as fl:
+                    for i in ldata:
+                        if(i == ldata[len(ldata) - 1]):
+                            fl.write(i)
+                        else:
+                            fl.write(i + ',')
+
+#delete user data from loan request file after approving loan
+    with open("loan_request.txt","r") as f:
+        lines = f.readlines()
+        num = 0
+        for i in lines:
+            num = num + 1
+            if(i.split(',')[0] == cust_ID):
+                del lines[num -1]
+
+                with open("loan_request.txt","w") as file:
+                    for line in lines:
+                        file.write(line)
+
 
 def generateLoanID():
     with open('loan.txt', 'r') as file:
--- test1.py
+++ test1.py
@@ -43,36 +43,51 @@ import admin as ad
 #     else:
 #         value = data_db[-1].split(',')
 #         print(value[0])
-
-print("................Loan request..............\n")
-with open("loan_request.txt",'r') as fp:
-    heading = ['UID', 'LoanType', 'DATE', 'AMOUNT']
-    for i in heading:
-        if (i == heading[len(heading) - 1]):
-            print(i.center(20))
-        else:
-            print(i.center(20), end="")
-    print('\t------------------------------------------------------------------------')
-    for data_db in fp:
-        list_data = data_db.split(',')
-        for data in list_data:
-            if (data == list_data[len(list_data) - 1]):
-                print(data.center(20))
-            else:
-                print(data.center(20), end="")
-
-
-print("\nApprove Loan using respective user ID\n")
-cust_ID = input("Enter customer id: ")
-with open("loan_request.txt","r") as file:
-    for value in file:
-        ldata = value.split(',')
-        if cust_ID in ldata:
-            loanID = str(ad.generateLoanID())
-            ldata.insert(0,loanID)
-            with open('loan.txt','a') as fl:
-                for i in ldata:
-                    if(i == ldata[len(ldata) - 1]):
-                        fl.write(i)
-                    else:
-                        fl.write(i + ',')
+# def displaLoanRequest():
+#     print("................Loan request..............\n")
+#     with open("loan_request.txt",'r') as fp:
+#         heading = ['UID', 'LoanType', 'DATE', 'AMOUNT']
+#         for i in heading:
+#             if (i == heading[len(heading) - 1]):
+#                 print(i.center(20))
+#             else:
+#                 print(i.center(20), end="")
+#         print('\t------------------------------------------------------------------------')
+#         for data_db in fp:
+#             list_data = data_db.split(',')
+#             for data in list_data:
+#                 if (data == list_data[len(list_data) - 1]):
+#                     print(data.center(20))
+#                 else:
+#                     print(data.center(20), end="")
+#     acceptLoan()
+#
+# def acceptLoan():
+#     print("\nApprove Loan using respective user ID\n")
+#     cust_ID = input("Enter customer id: ")
+#     with open("loan_request.txt","r") as file:
+#         for value in file:
+#             ldata = value.split(',')
+#             if cust_ID in ldata:
+#                 loanID = str(ad.generateLoanID())
+#                 ldata.insert(0,loanID)
+#                 with open('loan.txt','a') as fl:
+#                     for i in ldata:
+#                         if(i == ldata[len(ldata) - 1]):
+#                             fl.write(i)
+#                         else:
+#                             fl.write(i + ',')
+#
+# #delete user data from loan request file after approving loan
+#     with open("loan_request.txt","r") as f:
+#         lines = f.readlines()
+#         num = 0
+#         for i in lines:
+#             num = num + 1
+#             if(i.split(',')[0] == cust_ID):
+#                 del lines[num -1]
+#
+#                 with open("loan_request.txt","w") as file:
+#                     for line in lines:
+#                         file.write(line)
+# displaLoanRequest()
