27bf913758585b7b2c463ab2615fd7c8bf33bb8f
Neerajan Dhakal <nirajandhakal634@gmail.com>
2023-01-17 23:24:06 +0545

    some changes

--- customer.py
+++ customer.py
@@ -1,5 +1,5 @@
 import main_file as mf
-import datetime
+from datetime import datetime
 import admin as ad
 def customerMenu(uid):
     print("1. Loan Details")
@@ -15,9 +15,9 @@ def customerMenu(uid):
     elif(choose == 2):
         pass
     elif (choose == 3):
-        pass
+        makeTransaction(uid)
     elif (choose == 4):
-        pass
+        customerLoanDetails(uid)
     elif (choose == 5):
         applyLoan(uid)
     elif (choose == 6):
@@ -50,22 +50,79 @@ def transaction(uid):
     customerLoanDetails(uid)
     print("\n")
     print("/t/t/t/t/tMAKE PAYMENT")
+    makeTransaction(uid)
 
+def makeTransaction(uid):
+    customerLoanDetails(uid)
     payment = int(input("AMOUNT: "))
     with open('loan.txt', 'r') as fp:
         for value in fp:
             list_value = value.split(',')
             if uid in list_value:
-                if(payment < list_value[-3]):
+                if (payment < int(list_value[-3]) and int(list_value[-2]) > payment):
                     print(f"You have yo pay minimum of {list_value[-3]}.")
-                elif(payment>list_value[-2]):
+                elif (payment > int(list_value[-2])):
                     print(f"You have to pay only {list_value[-2]}")
-                elif(payment<list_value[-2] or payment>list_value[-2]):
-                    pass
+                elif (payment < int(list_value[-2]) or payment > int(list_value[-2])):
+                    updateAmount(uid, payment)
+                    return
                 else:
                     print("Invalid Input, Try again....")
+                    makeTransaction(uid)
 
 
+
+def updateAmount(uid, payment):
+    with open('sample.txt', 'r') as fp:
+        file = fp.readlines()
+        print(file)
+
+        for value in file:
+            num = num + 1
+            list_value = value.split(',')
+            # print(list_value)
+            if (uid in list_value):
+                newTotalAmount = int(list_value[-2]) - payment
+                updateMonth = int(list_value[3]) - 1
+                if (updateMonth == 0 or newTotalAmount == 0):
+                    print("Congratulations, You have paid all the loan...")
+                    input("Press any key to continue.....")
+                    del file[num - 1]
+                    with open('sample.txt', 'w') as f:
+                        f.writelines(file)
+                        storeTransaction(list_value, str(newTotalAmount), str(payment))
+                        return
+                list_value[3] = str(updateMonth)
+                list_value[-2] = str(newTotalAmount)
+                string_value = ','.join([str(elem) for elem in list_value])
+                # print(string_value)
+                # print(num)
+                file[num - 1] = string_value
+                with open('sample.txt', 'w') as fn:
+                    fn.writelines(file)
+                    storeTransaction(list_value, str(newTotalAmount), str(payment))
+                    return
+
+
+def transactionIdGenerator():
+    with open('transaction.txt', 'r') as file:
+        data_db = file.readlines()
+        if(data_db == []):
+            return 1
+        else:
+            value = data_db[-1].split(',')
+            UID = value[0]
+            return int(UID)+1
+
+def storeTransaction(listData, amount, payment):
+    currentDateAndTime = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    tranctionData = [transactionIdGenerator(), listData[0], listData[1], listData[2], currentDateAndTime, payment, amount]
+    with open('transaction.txt', 'a') as fp:
+        for value in tranctionData:
+            if(value == tranctionData[-1]):
+                fp.write(value + ',' + '\n')
+            else:
+                fp.write(value + ',')
 
 def customerLoanDetails(uid):
     with open('loan.txt', 'r') as fp:
